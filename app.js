fe9573f45cbc4036662d82c16a953e23492ceddc
Msamson09 <michaelssamson@gmail.com>
2021-06-29 06:37:41 -0700

    Complete

--- app.js
+++ app.js
@@ -3,8 +3,34 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    }
+    
+    
+    playersGuess: null,
+    prevGuesses: [],
+    
+    getGuess: function () {
+        while (this.secretNum !== this.playersGuess){
+        this.playersGuess = Number(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+        
+        if (this.playersGuess > this.secretNum){
+          alert('Too high, try again')
+        } else if (this.playersGuess < this.secretNum) {
+          alert('Too low, try again')
+        }
+        
+      }
+      if (this.secretNum === this.playersGuess){
+        alert(`Winner winner chicken dinner!`)
+        
+      }
+      return
+    },
+    play: function () {
+      // this.secretNum = 
+      this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      
+      console.log(this.secretNum)
+      game.getGuess()
+    },
   }
+  game.play()
